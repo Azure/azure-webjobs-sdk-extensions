0f6e5f9eeebcf57fa089f884e83924bb7ed5d3cf
Mathew Charles <mathewc@microsoft.com>
2015-07-09 10:31:43 -0700

    AssemblyInfo fixes

--- test/WebJobs.Extensions.Tests/Properties/AssemblyInfo.cs
+++ test/WebJobs.Extensions.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using Xunit;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Azure.WebJobs.Extensions..Tests")]
+[assembly: AssemblyTitle("Microsoft.Azure.WebJobs.Extensions.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.Azure.WebJobs.Extensions..Tests")]
+[assembly: AssemblyProduct("Microsoft.Azure.WebJobs.Extensions.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
