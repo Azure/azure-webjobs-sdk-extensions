5fdda9bfe5fc08bee691e27b7a8c1943ae7cb971
Mathew Charles <mathewc@microsoft.com>
2015-08-21 17:24:59 -0700

    Project file fixes

--- src/WebJobs.Extensions.SendGrid/SendGridAttributeBindingProvider.cs
+++ src/WebJobs.Extensions.SendGrid/SendGridAttributeBindingProvider.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Net.Mail;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
