a2fe1ccbe2f67a4979effd8bb111fd97332c2f9c
Matt Mason <mamaso@microsoft.com>
2017-02-15 14:45:03 -0800

    Initial IoTHub Extension

---
+++ src/WebJobs.Extensions.IoTHub/MessageAsyncCollector.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace Microsoft.Azure.WebJobs.Extensions.IoTHub
+{
+    internal class MessageAsyncCollector : IAsyncCollector<Message>
+    {
+        private ServiceClient _client;
+        private string _deviceId;
+
+        public MessageAsyncCollector(ServiceClient client, string deviceId)
+        {
+            _client = client;
+            _deviceId = deviceId;
+        }
+
+        public Task AddAsync(Message item, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _client.SendAsync(_deviceId, item);
+        }
+
+        public Task FlushAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
