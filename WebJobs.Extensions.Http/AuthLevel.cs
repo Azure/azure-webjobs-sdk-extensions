6b80a9379c86f170c5d5c794148113a43ed2a684
Ahmed ElSayed <ahmels@microsoft.com>
2017-04-12 13:38:49 -0700

    Adding HttpTriggerAttribute

---
+++ WebJobs.Extensions.Http/AuthLevel.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace WebJobs.Extensions.Http
+{
+    /// <summary>
+    /// Enum for available authentication levels for an http triggered function.
+    /// </summary>
+    public enum AuthLevel
+    {
+        Anonymous,
+        Function,
+        Admin
+    }
+}
---
+++ WebJobs.Extensions.Http/HttpTriggerAttribute.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs.Description;
+
+namespace WebJobs.Extensions.Http
+{
+    /// <summary>
+    /// An attribute for defining http triggered functions
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter)]
+    [Binding]
+    public sealed class HttpTriggerAttribute : Attribute
+    {
+        /// <summary>
+        /// The function HTTP route template.
+        /// </summary>
+        public string Route { get; set; }
+
+        /// <summary>
+        /// The type of WebHook handled by the trigger (if handling a pre-defined WebHook).
+        /// </summary>
+        public string WebHookType { get; set; }
+
+        /// <summary>
+        /// The function HTTP authorization level.
+        /// </summary>
+        public AuthLevel AuthLevel { get; set; } = AuthLevel.Function;
+
+        /// <summary>
+        /// Allowed HTTP methods.
+        /// </summary>
+        public IEnumerable<HttpMethod> Methods { get; set; };
+    }
+}
