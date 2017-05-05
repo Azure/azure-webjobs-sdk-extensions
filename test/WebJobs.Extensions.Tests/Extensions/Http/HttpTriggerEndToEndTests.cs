09cab22fe74aec2390979cf84579503600e0e8a7
Mathew Charles <mathewc@microsoft.com>
2017-05-05 11:30:47 -0700

    Adding http e2e tests

---
+++ test/WebJobs.Extensions.Tests/Extensions/Http/HttpTriggerEndToEndTests.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions.Tests.Common;
+using Microsoft.Azure.WebJobs.Extensions.Timers;
+using Xunit;
+
+
+namespace Microsoft.Azure.WebJobs.Extensions.Tests.Extensions.Http
+{
+    public class HttpTriggerEndToEndTests
+    {
+        private JobHost _host;
+
+        public HttpTriggerEndToEndTests()
+        {
+            var config = new JobHostConfiguration();
+            config.UseHttp();
+            _host = new JobHost(config);
+        }
+
+        public void HttpToBlobTest()
+        {
+
+        }
+
+        public class RequestInfo
+        {
+            public string A { get; set; }
+            public string B { get; set; }
+        }
+
+        public static class TestFunctions
+        {
+            public static void HttpToBlob([HttpTrigger("get", "post")] RequestInfo request)
+            {
+
+            }
+        }
+    }
+}
