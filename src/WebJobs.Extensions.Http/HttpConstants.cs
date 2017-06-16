8eb550194265541c6091012c4dc621cb42ea770b
Mathew Charles <mathew.charles@microsoft.com>
2017-06-16 11:29:54 -0700

    EasyAuth Support

---
+++ src/WebJobs.Extensions.Http/HttpConstants.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.WebJobs.Extensions.Http
+{
+    internal static class HttpConstants
+    {
+        public const string AntaresEasyAuthProviderHeaderName = "X-MS-CLIENT-PRINCIPAL-IDP";
+        public const string AntaresEasyAuthIdentityProviderClaimName = "http://schemas.microsoft.com/identity/claims/identityprovider";
+    }
+}
