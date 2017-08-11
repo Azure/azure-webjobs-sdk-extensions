ae661e3c56742e3d0e9093b17d9788a1aadc2c9b
Matt Mason <mamaso@microsoft.com>
2017-08-11 09:43:18 -0700

    Original event hubs extension

---
+++ src/WebJobs.Extensions.EventHubs/EventHubTriggerInput.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.ServiceBus.Messaging;
+
+namespace Microsoft.Azure.WebJobs.ServiceBus
+{
+    // The core object we get when an EventHub is triggered. 
+    // This gets converted to the user type (EventData, string, poco, etc) 
+    internal sealed class EventHubTriggerInput      
+    {        
+        // If != -1, then only process a single event in this batch. 
+        private int _selector = -1;
+
+        internal EventData[] Events { get; set; }
+        internal PartitionContext PartitionContext { get; set; }
+
+        public static EventHubTriggerInput New(EventData eventData)
+        {
+            return new EventHubTriggerInput
+            {
+                PartitionContext = null,
+                Events = new EventData[]
+                {
+                      eventData
+                },
+                _selector = 0,
+            };
+        }
+
+        public bool IsSingleDispatch
+        {
+            get
+            {
+                return _selector != -1;
+            }
+        }
+
+        public EventHubTriggerInput GetSingleEventTriggerInput(int idx)
+        {
+            return new EventHubTriggerInput
+            {
+                Events = this.Events,
+                PartitionContext = this.PartitionContext,
+                _selector = idx
+            };
+        }
+
+        public EventData GetSingleEventData()
+        {
+            return this.Events[this._selector];
+        }        
+    }
+}
