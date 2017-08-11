2152a3e9b20ac3dfd9bd2b207e2f39efed66e4a6
Matt Mason <mamaso@microsoft.com>
2017-08-11 09:43:18 -0700

    Original event hubs extension

---
+++ src/WebJobs.Extensions.EventHubs/EventHubAttribute.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Azure.WebJobs.Description;
+
+namespace Microsoft.Azure.WebJobs.ServiceBus
+{
+    /// <summary>
+    /// Setup an 'output' binding to an EventHub. This can be any output type compatible with an IAsyncCollector.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
+    [Binding]
+    public sealed class EventHubAttribute : Attribute, IConnectionProvider
+    {
+        /// <summary>
+        /// Initialize a new instance of the <see cref="EventHubAttribute"/>
+        /// </summary>
+        /// <param name="eventHubName">Name of the event hub as resolved against the <see cref="EventHubConfiguration"/> </param>
+        public EventHubAttribute(string eventHubName)
+        {
+            this.EventHubName = eventHubName;
+        }
+
+        /// <summary>
+        /// The name of the event hub. This is resolved against the <see cref="EventHubConfiguration"/>
+        /// </summary>
+        [AutoResolve]
+        public string EventHubName { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the optional app setting name that contains the Event Hub connection string. If missing, tries to use a registered event hub sender.
+        /// </summary>
+        [AppSetting]
+        public string Connection { get; set; }
+    }    
+}
---
+++ src/WebJobs.Extensions.EventHubs/EventHubTriggerAttributeBindingProvider.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Azure.WebJobs.Host.Listeners;
+using Microsoft.Azure.WebJobs.Host.Triggers;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Microsoft.Azure.WebJobs.ServiceBus
+{
+    internal class EventHubTriggerAttributeBindingProvider : ITriggerBindingProvider
+    {
+        private readonly INameResolver _nameResolver;
+        private readonly EventHubConfiguration _eventHubConfig;
+        private readonly IConverterManager _converterManager;
+
+        public EventHubTriggerAttributeBindingProvider(
+            INameResolver nameResolver,
+            IConverterManager converterManager,
+            EventHubConfiguration eventHubConfig)
+        {
+            this._nameResolver = nameResolver;
+            this._converterManager = converterManager;
+            this._eventHubConfig = eventHubConfig;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+        public Task<ITriggerBinding> TryCreateAsync(TriggerBindingProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            ParameterInfo parameter = context.Parameter;
+            EventHubTriggerAttribute attribute = parameter.GetCustomAttribute<EventHubTriggerAttribute>(inherit: false);
+
+            if (attribute == null)
+            {
+                return Task.FromResult<ITriggerBinding>(null);
+            }
+
+            string resolvedEventHubName = _nameResolver.ResolveWholeString(attribute.EventHubName);
+
+            string consumerGroup = attribute.ConsumerGroup ?? EventHubConsumerGroup.DefaultGroupName;
+            string resolvedConsumerGroup = _nameResolver.ResolveWholeString(consumerGroup);
+
+            if (!string.IsNullOrWhiteSpace(attribute.Connection))
+            {
+                _eventHubConfig.AddReceiver(resolvedEventHubName, _nameResolver.Resolve(attribute.Connection));
+            }
+            
+            var eventHostListener = _eventHubConfig.GetEventProcessorHost(resolvedEventHubName, resolvedConsumerGroup);
+
+            Func<ListenerFactoryContext, bool, Task<IListener>> createListener =
+             (factoryContext, singleDispatch) =>
+             {
+                 IListener listener = new EventHubListener(factoryContext.Executor, eventHostListener, singleDispatch, _eventHubConfig);
+                 return Task.FromResult(listener);
+             };
+
+            ITriggerBinding binding = BindingFactory.GetTriggerBinding(new EventHubTriggerBindingStrategy(), parameter, _converterManager, createListener);
+            return Task.FromResult<ITriggerBinding>(binding);         
+        }
+    } // end class
+}
