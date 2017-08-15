2ce475e56ff9a95dbe34d2e55582a95879aeea89
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2017-08-15 13:29:09 -0700

    Fixing issues after rebase

--- src/WebJobs.Extensions.Http/Routing/IWebJobsRouter.cs
+++ src/WebJobs.Extensions.Http/Routing/IWebJobsRouter.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Routing;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Routing;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Http
 {
--- src/WebJobs.Extensions/Properties/AssemblyInfo.cs
+++ src/WebJobs.Extensions/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Microsoft.Azure.WebJobs.Extensions.Tests")]
