2b95bb5d50352202407d1cbdb27a24934e9f3ef2
Anthony Chu <anthony@anthonychu.ca>
2018-01-02 14:59:08 -0800

    Change test to test for both a string and nested parameter

--- test/WebJobs.Extensions.Tests/Extensions/CosmosDB/CosmosDBSqlResolutionPolicyTests.cs
+++ test/WebJobs.Extensions.Tests/Extensions/CosmosDB/CosmosDBSqlResolutionPolicyTests.cs
@@ -54,19 +54,20 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tests.Extensions.CosmosDB
         }
 
         [Fact]
-        public void TemplateBind_NestedParameter()
+        public void TemplateBind_StringParameterAndNestedParameter()
         {
             // Arrange
             PropertyInfo propInfo = null;
             CosmosDBAttribute resolvedAttribute = new CosmosDBAttribute();
             BindingTemplate bindingTemplate =
-                BindingTemplate.FromString("SELECT * FROM c WHERE c.id = {headers.x-ms-client-principal-name}");
+                BindingTemplate.FromString("SELECT * FROM c WHERE c.id = {id} and c.userId = {headers.x-ms-client-principal-name}");
             Dictionary<string, object> bindingData = new Dictionary<string, object>();
             bindingData.Add("headers", new Dictionary<string, string>
             {
                 { "x-ms-client-principal-name", "username" },
                 { "x-ms-client-principal-id", "userid" }
             });
+            bindingData.Add("id", "foo");
             CosmosDBSqlResolutionPolicy policy = new CosmosDBSqlResolutionPolicy();
 
             // Act
@@ -74,8 +75,9 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tests.Extensions.CosmosDB
 
             // Assert
             Assert.Single(resolvedAttribute.SqlQueryParameters, p => p.Name == "@headers_x_ms_client_principal_name" && p.Value.ToString() == "username");
-            Assert.Single(resolvedAttribute.SqlQueryParameters); // should not contain more parameters than required
-            Assert.Equal("SELECT * FROM c WHERE c.id = @headers_x_ms_client_principal_name", result);
+            Assert.Single(resolvedAttribute.SqlQueryParameters, p => p.Name == "@id" && p.Value.ToString() == "foo");
+            Assert.Equal(2, resolvedAttribute.SqlQueryParameters.Count); // should not contain more parameters than required
+            Assert.Equal("SELECT * FROM c WHERE c.id = @id and c.userId = @headers_x_ms_client_principal_name", result);
         }
     }
 }
