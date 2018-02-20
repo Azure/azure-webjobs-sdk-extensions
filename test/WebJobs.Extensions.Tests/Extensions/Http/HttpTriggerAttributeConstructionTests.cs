284545b727ace6852cd65f5b9b329795e2a96d14
Scott Baldric <scottmbaldric@gmail.com>
2018-02-19 19:55:09 -0600

    Adding license header.

--- test/WebJobs.Extensions.Tests/Extensions/Http/HttpTriggerAttributeConstructionTests.cs
+++ test/WebJobs.Extensions.Tests/Extensions/Http/HttpTriggerAttributeConstructionTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Azure.WebJobs.Extensions.Http;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Xunit;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Tests.Extensions.Http
