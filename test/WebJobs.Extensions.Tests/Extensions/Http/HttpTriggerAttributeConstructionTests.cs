8d46fc46169fbe16fb4c670ccceb8c422f4d613e
Scott Baldric <scottmbaldric@gmail.com>
2018-02-19 19:46:18 -0600

    Adding HttpTriggerAttribute AuthorizationLevel only constructor.

---
+++ test/WebJobs.Extensions.Tests/Extensions/Http/HttpTriggerAttributeConstructionTests.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Xunit;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Tests.Extensions.Http
+{
+    public class HttpTriggerAttributeConstructionTests
+    {
+        [Fact]
+        public void HttpTriggerAuthLevelCtor_NullMethods()
+        {
+            var trigger = new HttpTriggerAttribute(AuthorizationLevel.Admin);
+
+            Assert.Null(trigger.Methods);
+        }
+    }
+}
