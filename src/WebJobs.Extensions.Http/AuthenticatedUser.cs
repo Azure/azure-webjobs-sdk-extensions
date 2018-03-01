e28aac6708684fa500147f212050ee66f71ec4a0
Connor McMahon <connorm333@gmail.com>
2018-03-01 13:34:56 -0800

    Add easyauth information to a user parameter binding

---
+++ src/WebJobs.Extensions.Http/AuthenticatedUser.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Azure.WebJobs
+{
+    /// <summary>
+    /// A class containing information about the current authenticated user.
+    /// </summary>
+    public class AuthenticatedUser
+    {
+        /// <summary>
+        /// The JWT access token for the user
+        /// </summary>
+        [JsonProperty("access_token")]
+        public string AccessToken { get; private set; }
+
+        /// <summary>
+        /// The datetime that the user's access token expires
+        /// </summary>
+        [JsonProperty("expires_on")]
+        public DateTime ExpiresOn { get; private set; }
+
+        /// <summary>
+        /// The JWT token that represents the identity of the user.
+        /// </summary>
+        [JsonProperty("id_token")]
+        public string IdToken { get; private set; }
+
+        /// <summary>
+        /// The string representation of the identity provider for the authenticated user.
+        /// </summary>
+        [JsonProperty("provider_name")]
+        public string ProviderName { get; private set; }
+
+        /// <summary>
+        /// The datetime that the user's access token expires
+        /// </summary>
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; private set; }
+
+        [JsonProperty("user_claims")]
+        internal List<Claim> _userClaims { get; set; }
+
+        /// <summary>
+        /// An array of claims that apply to the authenticated user.
+        /// </summary>
+        [JsonIgnoreAttribute]
+        public IReadOnlyList<Claim> UserClaims
+        {
+            get
+            {
+                return this._userClaims.AsReadOnly();
+            }
+        } 
+
+        internal static AuthenticatedUser DeserializeJson(string json)
+        {
+            return JsonConvert.DeserializeObject<List<AuthenticatedUser>>(json, new JsonConverter[] { new ClaimConverter() })[0];
+        }
+
+        private class ClaimConverter : JsonConverter
+        {
+            public override bool CanConvert(Type objectType)
+            {
+                return objectType == typeof(Claim);
+            }
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                JObject jObject = JObject.Load(reader);
+                return new Claim(jObject["typ"].Value<string>(), jObject["val"].Value<string>());
+            }
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ src/WebJobs.Extensions.Http/AuthenticatedUserBindingProvider.cs
@@ -0,0 +1,168 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Azure.WebJobs.Host.Protocols;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Http
+{
+    internal class AuthenticatedUserBindingProvider : IBindingProvider
+    {
+        public Task<IBinding> TryCreateAsync(BindingProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            if (context.Parameter.ParameterType != typeof(AuthenticatedUser))
+            {
+                return Task.FromResult<IBinding>(null);
+            }
+
+            return Task.FromResult<IBinding>(new AuthenticatedUserBinding(context.Parameter));
+        }
+
+        private class AuthenticatedUserBinding : IBinding
+        {
+            private readonly ParameterInfo _parameter;
+
+            public AuthenticatedUserBinding(ParameterInfo parameter)
+            {
+                _parameter = parameter;
+            }
+
+            public bool FromAttribute => false;
+
+            public Task<IValueProvider> BindAsync(object value, ValueBindingContext context)
+            {
+                // TODO: Don't know what to do with this without binding data, or if it 
+                // is even needed in our use case.
+                throw new NotImplementedException("This method doesn't have binding data.");
+            }
+
+            public Task<IValueProvider> BindAsync(BindingContext context)
+            {
+                if (context == null)
+                {
+                    throw new ArgumentNullException("context");
+                }
+
+                return BindAsync(context.BindingData);
+            }
+
+            private static Task<IValueProvider> BindAsync(IReadOnlyDictionary<string, object> bindingData)
+            {
+                return Task.FromResult<IValueProvider>(new AuthenticatedUserValueProvider(bindingData));
+            }
+
+            public ParameterDescriptor ToParameterDescriptor()
+            {
+                return new ParameterDescriptor
+                {
+                    Name = _parameter.Name,
+                    DisplayHints = new ParameterDisplayHints
+                    {
+                        Description = "AuthenticatedUser"
+                    }
+                };
+            }
+
+            private class AuthenticatedUserValueProvider : IValueProvider
+            {
+                private static HttpClient _client = new HttpClient();
+                private IReadOnlyDictionary<string, object> _bindingData;
+
+                public AuthenticatedUserValueProvider(IReadOnlyDictionary<string, object> bindingData)
+                {
+                    _bindingData = bindingData;
+                }
+
+                public Type Type => typeof(AuthenticatedUser);
+
+                public async Task<object> GetValueAsync()
+                {
+                    var initialRequest = _bindingData.Values.FirstOrDefault(val => val.GetType() == typeof(HttpRequestMessage)) as HttpRequestMessage;
+                    if (initialRequest == null)
+                    {
+                        throw new InvalidOperationException("Cannot determine authenticated user without HttpTrigger.");
+                    }
+
+                    var authorizationHeader = GetAuthorizationHeaderValue(initialRequest);
+                    if (authorizationHeader != null)
+                    {
+                        return await GetAuthenticatedUserFromEasyAuth(authorizationHeader);
+                    }
+                    else
+                    {
+                        return GetAuthenticatedUserFromFunctionKey();
+                    }   
+                }
+
+                private static string GetAuthorizationHeaderValue(HttpRequestMessage initialRequest)
+                {
+                    var idTokenHeaders = initialRequest.Headers.Where(header => header.Key.EndsWith("id-token", StringComparison.OrdinalIgnoreCase));
+                    if (!idTokenHeaders.Any())
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        var idTokenHeader = idTokenHeaders.First();
+                        var idTokenValue = idTokenHeader.Value.First();
+                        return "Bearer " + idTokenValue;
+                    }
+                }
+
+                private async Task<AuthenticatedUser> GetAuthenticatedUserFromEasyAuth(string authorizationHeader)
+                {
+                    string hostname = Environment.GetEnvironmentVariable("WEBSITE_HOSTNAME");
+                    var authUri = "https://" + hostname + "/.auth/me";
+                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, authUri);
+                    request.Headers.Add("Authorization", authorizationHeader);
+                    HttpResponseMessage response = await _client.SendAsync(request);
+
+                    if (response.StatusCode == HttpStatusCode.Unauthorized)
+                    {
+                        // TODO: Determine if an exception is the most appropriate use case here
+                        // especially once we start dealing with AND/OR of multiple AuthLevel enums
+                        throw new InvalidOperationException("No user was authenticated.");
+                    }
+                    else if (response.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        //Same concern as the unauthorized case
+                        throw new InvalidOperationException("Authentication/Authorization is not enabled.");
+                    }
+                    else if (response.IsSuccessStatusCode)
+                    {
+                        string authenticatedUserJson = await response.Content.ReadAsStringAsync();
+                        return AuthenticatedUser.DeserializeJson(authenticatedUserJson);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException($"Do not know how to handle response status code {response.StatusCode} from {authUri}");
+                    }
+                }
+
+                private static AuthenticatedUser GetAuthenticatedUserFromFunctionKey()
+                {
+                    throw new NotImplementedException("TODO: Have not worked out what information I can/should populate here.");
+                }
+
+                public string ToInvokeString()
+                {
+                    //TODO: what goes here?
+                    return string.Empty;
+                }
+            }
+        }
+    }
+}
