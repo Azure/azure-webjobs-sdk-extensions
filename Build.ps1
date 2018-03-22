693cd2c514ab06fec6bcbb8b80a4cbe33b88458f
Brett Samblanet <brettsam@microsoft.com>
2018-03-22 09:22:35 -0700

    updating AppVeyor build

--- Build.ps1
+++ Build.ps1
@@ -1,10 +1,29 @@
-﻿if (Test-Path "env:ProgramFiles(x86)")
-{
-    $ProgramFiles = "${env:ProgramFiles(x86)}"
-}
-else
-{
-    $ProgramFiles = "$env:ProgramFiles"
-}
+﻿param (
+  [string]$packageSuffix = "0"
+)
 
-& "$ProgramFiles\MSBuild\12.0\Bin\MSBuild.exe" WebJobs.Extensions.proj $Args
+dotnet --version
+
+dotnet build -v q
+
+if (-not $?) { exit 1 }
+
+$projects =
+    "WebJobs.Extensions",
+    "WebJobs.Extensions.CosmosDB",
+    "WebJobs.Extensions.Http",
+    "WebJobs.Extensions.MobileApps",
+    "WebJobs.Extensions.Twilio",
+    "WebJobs.Extensions.SendGrid"
+
+foreach ($project in $projects)
+{
+  $cmd = "pack", "src\$project\$project.csproj", "-o", "..\..\buildoutput", "--no-build"
+  
+  if ($packageSuffix -ne "0")
+  {
+    $cmd += "--version-suffix", "-$packageSuffix"
+  }
+  
+  & dotnet $cmd  
+}
