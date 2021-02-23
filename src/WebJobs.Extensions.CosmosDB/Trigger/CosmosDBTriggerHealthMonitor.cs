7a278174486a3537ee8fcbe47b32cd279bfe5ac0
Matias Quaranta <ealsur@hotmail.com>
2021-02-23 09:07:00 -0800

    Improve log messages for health

--- src/WebJobs.Extensions.CosmosDB/Trigger/CosmosDBTriggerHealthMonitor.cs
+++ src/WebJobs.Extensions.CosmosDB/Trigger/CosmosDBTriggerHealthMonitor.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Threading.Tasks;
+using Microsoft.Azure.Documents.ChangeFeedProcessor.Exceptions;
 using Microsoft.Azure.Documents.ChangeFeedProcessor.Monitoring;
 using Microsoft.Extensions.Logging;
 
@@ -21,13 +22,20 @@ namespace Microsoft.Azure.WebJobs.Extensions.CosmosDB
             switch (record.Severity)
             {
                 case HealthSeverity.Critical:
-                    this._logger.LogCritical(record.Exception, $"Unhealthiness detected in the operation {record.Operation} for {record.Lease}.");
+                    this._logger.LogCritical(record.Exception, $"Critical error detected in the operation {record.Operation} for {record.Lease}.");
                     break;
                 case HealthSeverity.Error:
-                    this._logger.LogError(record.Exception, $"Unhealthiness detected in the operation {record.Operation} for {record.Lease}.");
+                    if (record.Exception is LeaseLostException)
+                    {
+                        this._logger.LogWarning(record.Exception, $"Lease was lost during operation {record.Operation} for {record.Lease}. This is expected during scaling and briefly during initialization as the leases are rebalanced across instances.");
+                    }
+                    else
+                    {
+                        this._logger.LogError(record.Exception, $"{record.Operation} encountered an error for {record.Lease}.");
+                    }
                     break;
                 default:
-                    this._logger.LogTrace($"{record.Operation} on lease {record.Lease?.Id}, partition {record.Lease?.PartitionId} for owner {record.Lease?.Owner}");
+                    this._logger.LogTrace($"{record.Operation} on lease {record.Lease}.");
                     break;
             }
 
