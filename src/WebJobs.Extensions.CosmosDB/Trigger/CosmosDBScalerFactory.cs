87dc56e634080c6f9f1e9ba29dba80299b07df85
Vincent Chiang <vchiang@microsoft.com>
2023-01-19 13:17:26 -0500

    add method to validate and process trigger data

--- src/WebJobs.Extensions.CosmosDB/Trigger/CosmosDBScalerFactory.cs
+++ src/WebJobs.Extensions.CosmosDB/Trigger/CosmosDBScalerFactory.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
+using System;
 using System.Linq;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.WebJobs.Host.Scale;
@@ -40,6 +41,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.CosmosDB.Trigger
 
             TriggerData[] allTriggers = JsonConvert.DeserializeObject<TriggerData[]>(scalerContext.TriggerData);
             TriggerData targetTrigger = allTriggers.SingleOrDefault(x => x.FunctionName == scalerContext.FunctionId);
+            targetTrigger = ValidateAndProcessTriggerData(targetTrigger);
 
             CosmosClientOptions options = new CosmosClientOptions();
             CosmosDBTriggerAttribute attribute = new CosmosDBTriggerAttribute(targetTrigger.DatabaseName, targetTrigger.ContainerName) { MaxItemsPerInvocation = targetTrigger.MaxItemsPerInvocation };
@@ -54,8 +56,37 @@ namespace Microsoft.Azure.WebJobs.Extensions.CosmosDB.Trigger
             return (FunctionId: functionId, Container: container, LeaseContainer: leaseContainer, CosmosDBTriggerAttribute: attribute, LeaseContainerPrefix: leaseContainerPrefix, LoggerFactory: loggerFactory);
         }
 
+        private TriggerData ValidateAndProcessTriggerData(TriggerData triggerData)
+        {
+            // Validate required arguments
+            if (triggerData == null)
+            {
+                throw new ArgumentNullException(nameof(triggerData));
+            }
+            else if (string.IsNullOrEmpty(triggerData.Connection))
+            {
+                throw new ArgumentNullException(nameof(triggerData.Connection));
+            }
+            else if (string.IsNullOrEmpty(triggerData.DatabaseName))
+            {
+                throw new ArgumentNullException(nameof(triggerData.DatabaseName));
+            }
+            else if (string.IsNullOrEmpty(triggerData.ContainerName))
+            {
+                throw new ArgumentNullException(nameof(triggerData.ContainerName));
+            }
+
+            // Populate optional arguments
+            triggerData.LeaseConnection = triggerData.LeaseConnection ?? triggerData.Connection;
+            triggerData.LeaseContainerName = triggerData.LeaseContainerName ?? "leases";
+            triggerData.LeaseContainerPrefix = triggerData.LeaseContainerPrefix ?? string.Empty;
+
+            return triggerData;
+        }
+
         // Taken from: https://github.com/Azure/azure-webjobs-sdk-extensions/blob/dev/src/WebJobs.Extensions.CosmosDB/Trigger/CosmosDBTriggerAttribute.cs
-        // Extracting the attributes necessary to make scale decisions.
+        // Extracting only the attributes necessary to make scale decisions.
+        // Assumes the attributes have already been resolved if defined as an environment variable
         [JsonObject]
         private class TriggerData
         {
