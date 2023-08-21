f08701bef1b973c2598867a3693866a884a452ad
Sampo Kivistö <sampo.kivisto@innofactor.com>
2023-08-21 20:23:58 +0300

    Remove old dependencies by adding net6 target framework and ignore WebApiCompatShim code in net6

---
+++ src/WebJobs.Extensions.Http/Shim/IHttpRequestMessageFeature.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+#if NET6_0_OR_GREATER
+
+using System.Net.Http;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Http.Shim
+{
+    public interface IHttpRequestMessageFeature
+    {
+        HttpRequestMessage HttpRequestMessage { get; set; }
+    }
+}
+
+#endif
